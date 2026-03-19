4937c1d85e9d905f72c568a42c83d7503ad616e9
Valentina A. Rozo <varozo@bora.sciclone.wm.edu>
2026-03-19 02:11:40 -0400

    edits

--- assignments/assignment_06/scripts/01_download_data.sh
+++ assignments/assignment_06/scripts/01_download_data.sh
@@ -2,7 +2,9 @@
 
 # deleted the ?/download=1 because it looked incorrect
 
-cd ~/SUPERCOMPUTING/assignments/assignment_06/data
+cd ~/SUPERCOMPUTING/assignments/assignment_06/
+mkdir -p data/
+cd data/
 
 wget 'https://zenodo.org/records/15730819/files/SRR33939694.fastq.gz?download=1'
 mv 'SRR33939694.fastq.gz?download=1' SRR33939694.fastq.gz
